37429e4aab5f066c81a47cbdd9db4cdd39012e45
Mary1980cre <marianna.e@mail.ru>
2023-03-02 09:42:24 +0300

    commetr

---
+++ index.php
@@ -0,0 +1,476 @@
+<?php
+session_start();
+$users = 'admin';
+$pass = 'a029d0df84eb5549c641e04a9ef389e5';
+ if($_POST['submit']){
+ if($users == $_POST['user'] AND $pass == md5($_POST['pass']))
+{
+ $_SESSION['admin'] = $users;
+ header("Location: index.php");
+ exit;
+ }
+else echo '<p>Логин или пароль неверны!</p>';
+} 
+
+
+
+?>  
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Spa салон-Ростов-на-Дону</title>
+		<!-- For-Mobile-Apps -->
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<!-- //For-Mobile-Apps -->
+		
+		<!-- Custom-Stylesheet-Links -->	
+			<link  rel="stylesheet" href="css/bootstrap.css" type="text/css" media="all" />
+			<link rel="stylesheet"	href="css/style.css" type="text/css" media="all" /> 
+			<link rel="stylesheet" href="css/lightbox.css">
+		<!-- //Custom-Stylesheet-Links -->
+		
+		<!-- Web-Fonts -->
+			<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+			<link href='https://fonts.googleapis.com/css?family=Merienda:400,700' rel='stylesheet' type='text/css'>
+		<!-- //Web-Fonts -->
+	</head>
+	<body>
+		<div class="banner-agileits" id="banner">
+			<div class="header-w3l">
+				<div class="container">
+					<div class="logo">
+						<a href="index.html"><h2>Spa-салон</h2></a>
+					</div>
+					<!-- Navbar -->
+					<div class="total-navbar">
+						<nav class="navbar navbar-default">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+								<span class="sr-only">Навигация по сайту</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+
+							<div id="navbar" class="navbar-collapse navbar-right collapse hover-effect">
+								<ul class="nav navbar-nav">
+									<li class="active"><a href="#home" class="scroll">Главная</a></li>
+									<li><a href="#services" class="scroll">Услуги</a></li>
+									<li><a href="#specialize" class="scroll">Акции</a></li>
+									<li><a href="#gallery" class="scroll">Галерея</a></li>
+									<li><a href="#contact" class="scroll">Контакты</a></li>
+                  <li><a href="/login.php" target="_blank">Войти</a></li>
+								</ul>
+							</div>
+						</nav>
+					</div>
+			<!-- //Navbar -->
+				</div>
+			</div>
+			<div class="banner-text">
+				 <h2> Окунитесь в полный relax</h2>
+				  <h1>SPA - ЦЕНТР RESEDENSE</h1>
+				 <div class="arrow">
+					<a href="#contact" class="scroll"><img src="images/arrow.png" alt=" " /></a>
+				</div>
+			</div>
+		</div>
+		
+	   <div class="services" id="services">
+			<div class="container">
+			<div class="services-top">
+					<div class="col-md-6 col-sm-6 services-topleft">
+						<h3>Услуги</h3>
+					</div>
+					<div class="col-md-6 col-sm-6 services-topright">
+						<p>единственный в Ростове салон красоты и СПА с интерьерами из восточной сказки и немецким качеством уходов от люксового бренда Babor, с которым работает уже 10 лет.</p>
+					</div>
+					<div class="clearfix"></div>
+			</div>
+				<div class="col-md-3 col-sm-3 services-grid services-grid1">
+					<span class="glyphicon glyphicon-grain" aria-hidden="true"></span>
+					<h4>МАССАЖ</h4>
+					<p>Ознакомительная экскурсия по тайской деревне BAUNTY
+Посещение SPA-зоны, в которой проходит ритуал
+Знакомство с мастером и традициями Таиланда
+Распаривание в фитобочке
+Традиционный тайский массаж
+Тайское чаепитие </p>
+				</div>
+				<div class="col-md-3 col-sm-3 services-grid services-grid2">
+			
+					<span class="glyphicon glyphicon-leaf" aria-hidden="true"></span>
+					<h4>ЖЕМЧУЖИНА ВОСТОКА</h4>
+					<p>Ознакомительная экскурсия по тайской деревне BAUNTY, знакомство с мастером и традициями Таиланда
+Посещение SPA-зоны, в которой проходит ритуал
+Распаривание в фитобочке
+Фут-массаж бамбуковой палочкой
+Массаж лица травяными мешочками
+Массаж головы и лица
+Тайское чаепитие </p>
+				</div>
+				<div class="col-md-3 col-sm-3 services-grid services-grid3">
+					<span class="glyphicon glyphicon-bed" aria-hidden="true"></span>
+					<h4>СПОКОЙНАЯ ВОЛНА</h4>
+					<p>Ознакомительная экскурсия по тайской деревне BAUNTY, знакомство с мастером и традициями Таиланда
+Фитобочка со сбором целебных трав
+Массаж шеи, спины, плеч
+Релакс
+Тайское чаепитие</p>
+				</div>
+				<div class="col-md-3 col-sm-3 services-grid services-grid4">
+					<span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>
+					<h4>ФИРМЕННЫЙ SPA-РИТУАЛ</h4>
+					<p>Ознакомительная экскурсия по тайской деревне BAUNTY, знакомство с мастером и традициями Таиланда
+Посещение SPA-зоны, в которой проходит ритуал
+Распаривание в фитобочке
+Арома-ойл или традиционный тайский ритуал
+Скрабирование
+Фут-ритуал бамбуковой палочкой
+Тайское чаепитие </p>
+				</div>
+				
+				<div class="clearfix"></div>
+			</div>
+	   </div>
+	 
+	  
+	<div class="specialize" id="specialize">
+	
+	    <div class="container">
+		
+			<div class="col-md-6 spgrids">
+				<h3>Акции</h3>
+			    <h5 class="special">>> Арома-ойл массаж с тайскими маслами - 1000р.</h5>
+			    <h5>>> Foot-массаж бамбуковой палочкой с тайским бальзамом-2000р.</h5>
+				<h5>>> Slim-массаж с фруктовым скрабом из папайи-1500р.</h5>
+				<h5>>> Талассотерапия — аппликации с грязью Мертвого моря-1000р.</h5>
+				<h5>>> Фитобочка со сбором целебных трав-500р.</h5>
+	
+			</div>
+			<div class="container-sale">
+		<?php		
+$endOfDiscount = mktime(23,59,0); // дата окончания распродажи
+$now = time(); // текущее время
+$secondsRemaining = $endOfDiscount - $now; // оставшееся время
+
+define('SECONDS_PER_MINUTE', 60); // секунд в минуте
+define('SECONDS_PER_HOUR', 3600); // секунд в часу
+
+
+$hoursRemaining = floor($secondsRemaining / SECONDS_PER_HOUR); // часы до даты
+$secondsRemaining -= ($hoursRemaining * SECONDS_PER_HOUR);     //обновляем переменную
+
+$minutesRemaining = floor($secondsRemaining / SECONDS_PER_MINUTE); //минуты до даты
+$secondsRemaining -= ($minutesRemaining * SECONDS_PER_MINUTE);     //обновляем переменную
+
+echo("<h2>До окончания распродажи осталось <br> $hoursRemaining ч, $minutesRemaining мин, $secondsRemaining сек</h2>"); //печатаем сообщение
+
+
+
+?>
+		</div>
+			
+
+
+
+			<div class="col-md-6 specialize-right">
+				<img src="images/19.jpg" alt=" " />
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+							
+	<!-- gallery -->
+	<div class="gallery" id="gallery">
+		<div class="container">
+			<h3>Галерея</h3>
+			<div class="gallery-grids">
+				<div class="col-md-6 gallery-grid-left">
+					<div class="gallery-grid">
+						<div class="view view-eighth vie g1">
+							<a href="images/g10.jpg" data-lightbox="example-set" data-title="Lorem Ipsum is simply dummy the when an unknown galley of type and scrambled it to make a type specimen.">
+								<img src="images/g10.jpg" alt=" " class="img-responsive" />
+								<div class="mask rose">
+									<h4>Арома-ойл массаж с тайскими маслами</h4>	
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="gallery-grid-left-grid">
+						<div class="gallery-grid-left-grid-left">
+							<div class="gallery-grid-left-grid-left-grid">
+								<div class="view view-eighth g2">
+									<a href="images/g5.jpg" data-lightbox="example-set" data-title="Lorem Ipsum is simply dummy the when an unknown galley of type and scrambled it to make a type specimen.">
+										<img src="images/g5.jpg" alt=" " class="img-responsive" />
+										<div class="mask mask1">
+											<h4>Тайский традиционный массаж</h4>
+											
+										</div>
+									</a>
+								</div>
+							</div>
+							<div class="gallery-grid-left-grid-left-grid">
+								<div class="view view-eighth g3">
+									<a href="images/g11.jpg" data-lightbox="example-set" data-title="Lorem Ipsum is simply dummy the when an unknown galley of type and scrambled it to make a type specimen.">
+										<img src="images/g11.jpg" alt=" " class="img-responsive" />
+										<div class="mask mask1">
+											<h4>Slim-массаж с фруктовым скрабом из папайи</h4>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="gallery-grid-left-grid-right">
+							<div class="view view-eighth g4">
+								<a href="images/g7.jpg" data-lightbox="example-set" data-title="Lorem Ipsum is simply dummy the when an unknown galley of type and scrambled it to make a type specimen.">
+									<img src="images/g7.jpg" alt=" " class="img-responsive" />
+									<div class="mask mask2">
+										<h4>Обертывание с морскими водорослями ламинарии</h4>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="col-md-6 gallery-grid-right">
+					<div class="gallery-grid-right-grid">
+						<div class="gallery-grid-right1">
+							<div class="view view-eighth g5">
+								<a href="images/g1.jpg" data-lightbox="example-set" data-title="Lorem Ipsum is simply dummy the when an unknown galley of type and scrambled it to make a type specimen.">
+									<img src="images/g1.jpg" alt=" " class="img-responsive" />
+									<div class="mask mask1">
+										<h4>Ароматерапия</h4>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="gallery-grid-right2">
+							<div class="view view-eighth g6">
+								<a href="images/g3.jpg" data-lightbox="example-set" data-title="Lorem Ipsum is simply dummy the when an unknown galley of type and scrambled it to make a type specimen.">
+									<img src="images/g3.jpg" alt=" " class="img-responsive" />
+									<div class="mask mask1">
+										<h4>Финская сауна</h4>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="gallery-grid-right-grid">
+						<div class="gallery-grid-right1">
+							<div class="view view-eighth g7">
+								<a href="images/8.jpg" data-lightbox="example-set" data-title="Lorem Ipsum is simply dummy the when an unknown galley of type and scrambled it to make a type specimen.">
+									<img src="images/8.jpg" alt=" " class="img-responsive" />
+									<div class="mask mask2">
+										<h4>Фитобочка со сбором целебных трав</h4>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="gallery-grid-right2">
+							<div class="view view-eighth g8">
+								<a href="images/g8.jpg" data-lightbox="example-set" data-title="Lorem Ipsum is simply dummy the when an unknown galley of type and scrambled it to make a type specimen.">
+									<img src="images/g8.jpg" alt=" " class="img-responsive" />
+									<div class="mask mask2">
+										<h4>Снежная пещера «Ледяной грот»</h4>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="gallery-grid-right-grid">
+						<div class="gallery-grid-right1">
+							<div class="view view-eighth g9">
+								<a href="images/g9.jpg" data-lightbox="example-set" data-title="Lorem Ipsum is simply dummy the when an unknown galley of type and scrambled it to make a type specimen.">
+									<img src="images/g9.jpg" alt=" " class="img-responsive" />
+									<div class="mask mask1">
+										<h4>SPA-джакузи с гидромассажем</h4>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="gallery-grid-right2">
+							<div class="view view-eighth g10">
+								<a href="images/g2.jpg" data-lightbox="example-set" data-title="Lorem Ipsum is simply dummy the when an unknown galley of type and scrambled it to make a type specimen.">
+									<img src="images/g2.jpg" alt=" " class="img-responsive" />
+									<div class="mask mask1">
+										<h4>Тропический душ впечатлений</h4>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+				
+			</div>
+		</div>
+	</div>
+	<!-- //gallery -->
+   
+    <div class="full-image">
+		<div class="container">
+		   <div class="figrids-total">
+				<div class="col-md-4 col-sm-4 figrids figrid1">
+					<div class='numscroller numscroller-big-bottom' data-slno='1' data-min='0' data-max='3000' data-delay='.5' data-increment="1"></div>
+					<label class="line"></label>
+					<h5>Клиентов</h5>
+				</div>
+				<div class="col-md-4 col-sm-4 figrids figrid2">
+					<div class='numscroller numscroller-big-bottom' data-slno='1' data-min='0' data-max='1005' data-delay='.5' data-increment="1"></div>
+					<label class="line"></label>
+					<h5>Процедур</h5>
+				</div>
+				<div class="col-md-4 col-sm-4 figrids figrid3">
+					<div class='numscroller numscroller-big-bottom' data-slno='1' data-min='0' data-max='800' data-delay='.5' data-increment="1"></div>
+					<label class="line"></label>
+					<h5>Лечения</h5>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	
+	
+	<div class="staff" id="team">
+		<div class="container">
+			<h3>Команда</h3>
+			<div class="staff-top">
+		        <div class="col-md-3 col-sm-3 staff-img staff-img1">
+					<img src="images/15.jpg" alt=" " />
+				</div>
+				<div class="col-md-3 col-sm-3  staff-text  staff-text1">
+					<h4>Светлана Ходкова</h4>
+				    <h5>Spa-массажист </h5>
+					<p>Шоколадное, винное и медовое обертывания
+Slim-массаж с фруктовым скрабом из папайи</p>
+			    </div>
+			
+				<div class="col-md-3 col-sm-3  staff-img staff-img2">
+					<img src="images/12.jpeg" alt=" " />
+				</div>
+				<div class="col-md-3 col-sm-3  staff-text  staff-text2">
+					<h4>Кристина Агапова</h4>
+					<h5>Косметолог</h5>
+					<p>косметологические процедуры и услуг, как в сфере аппаратной, так и инъекционной косметологии.</p>
+			    </div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="staff-bottom">
+				<div class="col-md-3 col-sm-3  staff-img staff-img3">
+					<img src="images/13.jpg" alt=" " />
+				</div>
+				<div class="col-md-3 col-sm-3   staff-text  staff-text3">
+					<h4>Анна Трамова</h4>
+					<h5>Массажист </h5>
+					<p>Арома-ойл массаж с тайскими маслами
+Stonе-терапия «Живые камни» с базальтовой вулканической породой</p>
+			    </div>
+			
+				<div class="col-md-3 col-sm-3  staff-img staff-img4">
+					<img src="images/16.jpg" alt=" " />
+				</div>
+				<div class="col-md-3 col-sm-3  staff-text staff-text4">
+					<h4>Диана Громова</h4>
+					<h5>Spa-специалист</h5>
+					<p>SPA-джакузи с гидромассажем
+Тропический душ впечатлений
+Relax-зона «Татами»</p>
+			    </div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	
+	
+	<div class="contact" id="contact">
+		<div class="container">
+		    <div class="col-md-6 col-sm-6 contact-left">
+				<h4>Контакты</h4>
+					<div class="phone">
+						<h5>Телефон;</h5>
+						<h5>+78000000000</h5>
+					</div>
+					<div class="mail">
+						<h5>E-mail;</h5>
+						<h5><a href="mailto:info@example.com">mail@example.com</a></h5>
+					</div>
+					<div class="address">
+						<h5>Адрес;</h5>
+						<h5>Ростов-на-Дону, Доватора 54</h5>
+      </div>
+			</div>
+			<div class="col-md-6 col-sm-6 contact-right">
+				<form action="send.php" method="post">
+					<input type="text" name="name" placeholder="Ваше имя"><br>
+					<input type="text" name="phone" placeholder="Ваш номер телефона"><br>
+					<input type="text" name="email" placeholder="Ваш EMAIL"><br>
+					<textarea rows="4" cols="50" name="message" placeholder="Сообщение"></textarea>
+					<input type="submit"  class="hvr-rectangle-in" value="Отправить">
+					<div class="clearfix"></div>
+				</form>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	
+	<div class="footer-w3">
+		<div class="container">
+			<p class="agileinfo"> &copy; 2023 Spa-Салон. Все права защищены</p>
+		</div>
+	</div>
+  
+	
+
+  
+	
+	<!-- common js -->
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.js"></script>	
+	<!-- //js -->
+	<!-- script for gallery -->
+		<script src="js/lightbox-plus-jquery.min.js"> </script>
+	<!-- //script for gallery -->
+	
+	<!-- here stars scrolling icon -->
+		<script type="text/javascript">
+			$(document).ready(function() {
+				/*
+					var defaults = {
+					containerID: 'toTop', // fading element id
+					containerHoverID: 'toTopHover', // fading element hover id
+					scrollSpeed: 1200,
+					easingType: 'linear' 
+					};
+				*/
+									
+				$().UItoTop({ easingType: 'easeOutQuart' });
+									
+				});
+		</script>
+	<!-- //here ends scrolling icon -->
+
+	<!-- Script For Number Scrolling -->
+		<script type="text/javascript" src="js/numscroller-1.0.js"></script>
+	<!-- //Script For Number Scrolling -->
+
+	<!-- start-smooth-scrolling -->
+			<script type="text/javascript" src="js/move-top.js"></script>
+			<script type="text/javascript" src="js/easing.js"></script>
+			<script type="text/javascript">
+				jQuery(document).ready(function($) {
+					$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+				});
+			});
+			</script>
+	<!-- //start-smoth-scrolling -->
+
+	</body>
+</html>
